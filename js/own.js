567de693389f39b08e25dfed9ffa8c6dcb9140e3
Arman <galamat.aa@gmail.com>
2020-03-04 14:43:04 +0600

    Git init

---
+++ js/own.js
@@ -0,0 +1,14 @@
+/*$(document).ready(function(){
+	var counter = 0;
+	var a = 0;
+	var b = setInterval(function(){
+		$("loaderpre.numberpre.counted").html(a);
+		$("loaderpre").css("width", a + "%");
+		counter++;
+		a++;
+		if(counter == 101){
+			clearInterval(b);
+			$(".loaderpre").css("display","none");
+		}
+	}, 10);
+});*/
